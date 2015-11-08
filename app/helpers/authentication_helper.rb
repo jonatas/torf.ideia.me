c3be487a224067a547ee95a5cbcc0b4f4054f36b
Jônatas Davi Paganini <jonatasdp@gmail.com>
2015-11-08 03:58:54 -0200

    refactoring

--- app/helpers/authentication_helper.rb
+++ app/helpers/authentication_helper.rb
@@ -3,15 +3,25 @@ module AuthenticationHelper
     session[:user_data].present?
   end
 
+   AUTHORIZE_URL = "https://github.com/login/oauth/authorize?"
+
+  def callback_url
+    host = 
+      if Rails.env.production?
+        "torf.r15.railsrumble.com"
+      else
+        "localhost:3000"
+      end
+    "http://#{host}/users/auth/github/callback"
+  end
+
   def github_sign_in_path
-    if Rails.env.production?
-      host = "torf.r15.railsrumble.com"
-    else
-      host = "localhost:3000"
-    end
-    "https://github.com/login/oauth/authorize" +
-    "?client_id=#{ENV["GITHUB_APP_ID"]}" +
-    "&redirect_url=http://#{host}/users/auth/github/callback" +
-    "&state=#{SecureRandom.urlsafe_base64}"
+    _params = {
+      client_id: ENV["GITHUB_APP_ID"],
+      redirect_url: callback_url,
+      state: SecureRandom.urlsafe_base64
+    }
+
+    AUTHORIZE_URL + _params.map { |i| i * "="  } * "&"
   end
 end
